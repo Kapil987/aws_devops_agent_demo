825f4ebc492a9fa944c2012dc4010617f6b3a2d1
Kapil <kapil0123@gmail.com>
2026-01-16 14:09:32 +0530

    bugfix: renamed the lambda funciton

--- src/ldevopsagent.py
+++
@@ -1,9 +0,0 @@
-# lambda_function.py
-import logging
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def lambda_handler(event, context):
-    # forced failure
-    raise Exception("Intentional failure for DevOps Agent test")
